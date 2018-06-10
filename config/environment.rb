2b17db069eb0be2d6965642ea0f828fe14326caf
Matt Farmer <mrfarmer777@gmail.com>
2018-06-10 18:51:33 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
-require 'bundler'
-Bundler.require
+require 'bundler' #requies the undler gem
+Bundler.require #uses bundler to pull in all other gems
 
-require_relative '../lib/student'
+require_relative '../lib/student' #requires the student class from lib
 
-DB = {:conn => SQLite3::Database.new("db/students.db")}
+DB = {:conn => SQLite3::Database.new("db/students.db")} #initializes a database connection
