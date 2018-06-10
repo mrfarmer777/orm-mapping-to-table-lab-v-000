37caba2bf12f15cbdaccab07bbf9feaac4182804
Matt Farmer <mrfarmer777@gmail.com>
2018-06-10 18:57:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
   #  with DB[:conn]
 
   def self.create_table
-    sql=<<- SQL
+    sql=<<-SQL
       CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
