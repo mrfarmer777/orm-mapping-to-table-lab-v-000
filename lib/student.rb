d695f37c7f6d316d7f534f9ea1db2b9ca4c47add
Matt Farmer <mrfarmer777@gmail.com>
2018-06-10 18:59:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,13 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  def initialize(name,grade,id=nil)
+    @name=name
+    @grade=grade
+  end
+  
+
+  end
   def self.create_table
     sql=<<-SQL
       CREATE TABLE IF NOT EXISTS students (
